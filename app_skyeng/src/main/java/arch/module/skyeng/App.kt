25a770d5fce02ad5312aa172eb8d622b077f56af
zahar <zahar.zolotarev@gmail.com>
2019-07-24 10:26:29 +0300

    Создал проект

---
+++ app_skyeng/src/main/java/arch/module/skyeng/App.kt
@@ -0,0 +1,7 @@
+package arch.module.skyeng
+
+import android.app.Application
+
+
+class App : Application() {
+}
---
+++ app_skyeng/src/main/java/arch/module/skyeng/ui/main/SkyengActivity.kt
@@ -0,0 +1,14 @@
+package arch.module.skyeng.ui.main
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import arch.module.skyeng.R
+
+
+class SkyengActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.layout_skyeng)
+    }
+}
